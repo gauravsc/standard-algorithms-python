9fa2344fb45a26d3deb2c70b1950b5de113d6270
Gaurav Singh <gauravonline20@gmail.com>
2012-07-25 15:54:07 +0530

    added dfs, bfs and updated readme

--- dfs.py
+++ dfs.py
@@ -1,6 +1,4 @@
-#It is an implementation of breadth first search
-
-from collections import deque
+#It is an implementation of depth first search
 
 class node:
     def __init__(self):
@@ -9,21 +7,21 @@ class node:
 
 
 
-def find_element(element_to_find, queue):
-    if  len(queue) < 1:
+def find_element(element_to_find, stack):
+    if  len(stack) < 1:
         return ""
-    ptr=queue.popleft();
+    ptr=stack.pop();
     if ptr.element==element_to_find:
         return ptr
     else:      
     	for i in ptr.children:
             child=ptr.children[i]
-	    queue.append(child)
-    return find_element(element_to_find, queue)
+	    stack.append(child)
+    return find_element(element_to_find, stack)
 
 
 #code below is for initiliazing the tree for bfs search
-queue=deque()
+stack=list()
 root= node()
 root.children[1]=node()
 root.children[1].element=1
@@ -31,8 +29,8 @@ root.children[2]=node()
 root.children[2].element=2
 root.children[1].children[1]=node()
 root.children[1].children[1].element=3
-queue.append(root)
-a= find_element(6, queue)
+stack.append(root)
+a= find_element(3, stack)
 if a:
     print a.element 
 else:
